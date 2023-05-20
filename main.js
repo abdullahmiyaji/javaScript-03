023f18e8f67ca946824b80b670f3d3afb0affad7
abdullah-011 <you@example.com>
2023-05-21 02:14:42 +0300

    <add code>

---
+++ main.js
@@ -0,0 +1,33 @@
+/*Assignments-03
+1.. Find GPA and Grade with respect to Marks in any subject
+*/
+
+let marks = -30;
+
+if (marks >= 90 && marks <= 100) {
+      console.log(`your Marks is = ${marks}  and Grade is = A+`)
+}
+else if (marks >= 80 && marks <= 89) {
+      console.log(`your Marks is = ${marks}  and Grade is = A`)
+}
+else if (marks >= 70 && marks <= 79) {
+      console.log(`your Marks is = ${marks}  and Grade is = A-`)
+}
+else if (marks >= 60 && marks <= 69) {
+      console.log(`your Marks is = ${marks}   and Grade is = B`)
+}
+else if (marks >= 50 && marks <= 59) {
+      console.log(`your Marks is = ${marks}  and Grade is = C`)
+}
+else if (marks >= 33 && marks <= 49) {
+      console.log(`your Marks is = ${marks}  and Grade is = D`)
+}
+else if (marks >= 0 && marks <= 32) {
+      console.log(`your Marks is = ${marks} and Grade is F`)
+} else {
+      console.log("wrong number")
+} 
+
+
+
+//  hello i am push code
